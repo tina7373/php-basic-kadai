fd25e6a8939056c751969841576e847e06415a19
tina7373 <chinatsu.sora.cute@gmail.com>
2025-01-03 16:24:13 +0900

    Add files via upload

---
+++ kadai_005/score.php
@@ -0,0 +1,30 @@
+<!-- 生徒10人分の点数を入れる変数と値の定義 -->
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>課題3~5章</title>
+</head>
+<body>
+    <p>
+        <?php
+        $score1 = "80";
+        $score2 = "60";
+        $score3 = "55";
+        $score4 = "40";
+        $score5 = "100";
+        $score6 = "25";
+        $score7 = "80";
+        $score8 = "95";
+        $score9 = "30";
+        $score10 = "60";
+
+        echo $sam_score = $score1 + $score2 + $score3 + $score4 + $score5 + $score6 + $score7 + $score8 + $score9 + $score10;
+        echo "<br>";
+        echo $avarage_score = $sam_score / 10;
+        ?>
+    </p>
+</body>
+</html> 
+
