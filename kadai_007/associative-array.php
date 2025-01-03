eab311c8111b8f6a27a11d1b334bd75ec19c74f7
tina7373 <chinatsu.sora.cute@gmail.com>
2025-01-03 16:53:19 +0900

    Add files via upload

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>課題6~8章</title>
+</head>
+<body>
+    <p>
+        <?php
+        //連想配列の定義
+        $product_data = ['name' => 'onion', 'price' => 200, 'weight' => 160];
+        print_r($product_data);
+        ?>
+    </p>
+</body>
+</html>
